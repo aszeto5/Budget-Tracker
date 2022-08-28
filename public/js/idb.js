16576dd03ac5c9d9c890b03ff9d161257bba4853
Adrian Duong Szeto <adrianduongszeto@gmail.com>
2022-08-28 03:42:20 -0700

    trying deploy on heroku

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,59 +1,78 @@
 let db;
-const request = indexedDB.open('budget_tracker', 1);
+// connection to IndexedDB database
+const request = indexedDB.open("budget_tracker", 1);
 
 request.onupgradeneeded = function (event) {
-    const db = event.target.result;
-    db.createObjectStore('new_transaction', { autoIncrement: true });
+  // save a reference to the database
+  const db = event.target.result;
+
+  db.createObjectStore("new_transaction", { autoIncrement: true });
 };
 
+// when successful
 request.onsuccess = function (event) {
-    db = event.target.result;
-    if (navigator.onLine) {
-        uploadTransaction();
-    }
+  db = event.target.result;
+
+  //if app is online run uploadTransaction to send local db data to api
+  if (navigator.onLine) {
+    uploadTransaction();
+  }
 };
 
 request.onerror = function (event) {
-    console.log(event.target.errorCode);
+  // error
+  console.log(event.target.errorCode);
 };
 
+// open a new transaction with read and write permissions
 function saveRecord(record) {
-    const transaction = db.transaction(['new_transaction'], 'readwrite');
-    const budgetObjectStore = transaction.objectStore('new_transaction');
-    budgetObjectStore.add(record);
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+
+  const transactionObjectStore = transaction.objectStore("new_transaction");
+
+  transactionObjectStore.add(record);
 }
 
+// open a new transaction to the database to read the data
 function uploadTransaction() {
-    const transaction = db.transaction(['new_transaction'], 'readwrite');
-    const budgetObjectStore = transaction.objectStore('new_transaction');
-    const getAll = budgetObjectStore.getAll();
+  const transaction = db.transaction(["new_transaction"], "readwrite");
 
-    getAll.onsuccess = function () {
+  const transactionObjectStore = transaction.objectStore("new_transaction");
 
-        if (getAll.result.length > 0) {
-            fetch('/api/transaction', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(response => response.json())
-                .then(serverResponse => {
-                    if (serverResponse.message) {
-                        throw new Error(serverResponse);
-                    }
-                    const transaction = db.transaction(['new_transaction'], 'readwrite');
-                    const budgetObjectStore = transaction.objectStore('new_transaction');
-                    budgetObjectStore.clear();
-                    alert('All saved transactions has been submitted!');
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        }
+  const getAll = transactionObjectStore.getAll();
+
+  // when successful getAll function will run
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(["new_transaction"], "readwrite");
+
+          const transactionObjectStore =
+            transaction.objectStore("new_transaction");
+          // clear all items
+          transactionObjectStore.clear();
+
+          alert("All saved transactions have been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
+  };
 }
 
-window.addEventListener('online', uploadTransaction);
+// listen for when app comes back online
+window.addEventListener("online", uploadTransaction);
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-app";
 
 const app = express();
 
@@ -16,14 +16,22 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
+mongoose.connect(
+  MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    // useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) console.log(err);
+    else console.log("mongodb running");
+  }
+);
+console.log("Connecting to MONgo");
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}! http://localhost:${PORT}`);
 });
